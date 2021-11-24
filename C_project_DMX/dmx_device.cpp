60f410206d1b80edaf289e263a129053e65512e0
Tibo Lambrechts <12000174@student.pxl.be>
2021-11-24 17:46:19 +0100

    atribute toevoegen

--- C_project_DMX/dmx_device.cpp
+++ C_project_DMX/dmx_device.cpp
@@ -1,11 +1,15 @@
 #include "dmx_device.h"
+#include <string>
 
 DMX_Device::DMX_Device()
 {
 
-    class MyClass
+    class DMX_Device
     {
       public:
+        int fixture_Num;
+        bool On_Off;
+        int Adress;
 
 
     };
